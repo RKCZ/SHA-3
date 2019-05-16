2bd0db661c3f6f7891ada3907bb6989a8bb0c07b
rkcz <kalivodaroman@gmail.com>
2019-05-16 09:03:02 +0200

    final commit

--- src/keccak/keccak.py
+++ src/keccak/keccak.py
@@ -4,15 +4,12 @@ ROUND_COUNT = 24
 
 BYTE_1 = 0x80
 
-'''
-rotates 
-'''
 def rotateLeft(a, n):
     return ((a >> (INT_SIZE - (n % INT_SIZE))) + (a << (n % INT_SIZE))) % (1 << INT_SIZE)
 
 
 def KeccakRound(lanes):
-    R = 1
+    rc = 1
     for _ in range(ROUND_COUNT):
         C = [lanes[x][0] ^ lanes[x][1] ^ lanes[x][2] ^ lanes[x][3] ^ lanes[x][4] for x in range(5)]
         D = [C[(x + 4) % 5] ^ rotateLeft(C[(x + 1) % 5], 1) for x in range(5)]
@@ -30,24 +27,29 @@ def KeccakRound(lanes):
                 lanes[x][y] = T[x] ^ ((~T[(x + 1) % 5]) & T[(x + 2) % 5])
         # ι
         for j in range(7):
-            R = ((R << 1) ^ ((R >> 7) * 0x71)) % 256
-            if (R & 2):
+            rc = ((rc << 1) ^ ((rc >> 7) * 0x71)) % 256
+            if (rc & 2):
                 lanes[0][0] = lanes[0][0] ^ (1 << ((1 << j) - 1))
     return lanes
 
-
+# load bits in lane from state into single byte
 def load(b):
     return sum((b[i] << (BYTE_SIZE * i)) for i in range(BYTE_SIZE))
 
-
+# convert bits from single byte into list
 def store(a):
     return list((a >> (BYTE_SIZE * i)) % 256 for i in range(BYTE_SIZE))
 
 
 def KeccakF(state):
+    # create lanes from received state
     lanes = [[load(state[BYTE_SIZE * (x + 5 * y):BYTE_SIZE * (x + 5 * y) + BYTE_SIZE]) for y in range(5)] for x in range(5)]
+    
+    # run permutations on lanes
     lanes = KeccakRound(lanes)
     state = bytearray(200)
+    
+    # put permutated lanes back into state
     for x in range(5):
         for y in range(5):
             state[BYTE_SIZE * (x + 5 * y):BYTE_SIZE * (x + 5 * y) + BYTE_SIZE] = store(lanes[x][y])
@@ -70,22 +72,28 @@ def KeccakSponge(capacity, inputBytes, delimitedSuffix, outputByteLen, bitLength
     inputOffset = 0
     # absorb
     while(inputOffset < len(inputBytes)):
+        # if remaining input is smaller than rate
         blockSize = min(len(inputBytes) - inputOffset, rateInBytes)
-        for i in range(blockSize):
-            
+        
+        for i in range(blockSize):            
             # xor current state with absorbing message
             state[i] = state[i] ^ inputBytes[i + inputOffset]
+            
+            #increment offset
         inputOffset = inputOffset + blockSize
         if (blockSize == rateInBytes):
             state = KeccakF(state)
             blockSize = 0
-    # padding
+    # add hash specific padding to the end of input
     state[blockSize] = state[blockSize] ^ delimitedSuffix
-    if (((delimitedSuffix & BYTE_1) != 0) and (blockSize == (rateInBytes - 1))):
+    
+    # hash remaining input
+    if (blockSize == (rateInBytes - 1)):
         state = KeccakF(state)
     state[rateInBytes - 1] = state[rateInBytes - 1] ^ BYTE_1
     state = KeccakF(state)
-    # squeeze
+    
+    # squeeze output of given length
     while(outputByteLen > 0):
         blockSize = min(outputByteLen, rateInBytes)
         outputBytes = outputBytes + state[0:blockSize]
